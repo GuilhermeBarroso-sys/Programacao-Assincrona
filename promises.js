ad1cd9fe1bc4e603659c1c60e6e233f7c8e42f20
GuilhermeBarroso-Sys <guilhermebarrosodeoliveira@outlook.com>
2021-02-09 14:36:58 -0300

    Adicionando promises

---
+++ promises.js
@@ -0,0 +1,46 @@
+function getId() {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve({id : 5})
+        },2000)
+    })
+}
+
+function consultID(id) {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve("gui@hotmail.com");
+        },2000);
+    })
+}
+
+
+
+function sendEmail(body, to) {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+           var erro = false;
+
+           if(!erro) {
+               resolve({time:6 , to: to })
+           }
+           else {
+               reject("Invalid Body!") 
+           }
+        },2000);
+    })
+}
+
+
+getId().then(({id}) => {
+    consultID(id).then((email) => {
+        sendEmail("Hello World!", email).then(({time, to}) => {
+            console.log(`
+                    Sucess! Email has been send!
+                    info:
+                    Time: ${time}
+                    To: ${to}
+                `)
+        })
+    })
+})
